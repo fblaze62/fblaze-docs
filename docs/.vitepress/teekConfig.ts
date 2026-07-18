9306f15c2355319026e193320657185b57db2c0e
fblaze62 <feablaze@outlook.com>
2026-07-18 15:58:57 +0800

    Update teekConfig to enhance Waline comment system settings and enable visitor count

--- docs/.vitepress/teekConfig.ts
+++ docs/.vitepress/teekConfig.ts
@@ -41,6 +41,21 @@ export const teekConfig = defineTeekConfig({
       serverURL: "https://cmts.docs.fblaze62.top",
       jsLink: "https://unpkg.com/@waline/client@v3/dist/waline.js",
       cssLink: "https://unpkg.com/@waline/client@v3/dist/waline.css",
+      // 设置 Waline 评论系统语言
+      lang: "zh-CN",
+      // 设置 Waline 评论系统表情地址
+      emoji: [
+        "https://unpkg.com/@waline/emojis@1.4.0/weibo",
+        "https://unpkg.com/@waline/emojis@1.4.0/bilibili",
+        "https://unpkg.com/@waline/emojis@1.4.0/bmoji",
+      ],
+      // 评论登录模式。可选值如下：
+      //   'enable'   —— 默认，允许访客匿名评论和用第三方 OAuth 登录评论，兼容性最佳。
+      //   'force'    —— 强制必须登录后才能评论，适合严格社区，关闭匿名评论。
+      //   'disable'  —— 禁止所有登录和 OAuth，仅允许匿名评论（填写昵称/邮箱），适用于极简留言。
+      login: "force",
+      // 是否启用文章访问量统计功能
+      visitorCount: true,
 
       // giscus 配置，官网：https://giscus.app/zh-CN
       // repo: "your name/your repo",
